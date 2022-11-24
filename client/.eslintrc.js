f9640af9d52662c365179d5987b30edef2f94981
jintak0401 <32933980+jintak0401@users.noreply.github.com>
2022-11-24 22:13:46 +0900

    프로젝트 초기 구성 (#2)
    * chore: 필요한 라이브러리 추가 및 tailwind 설정
    
    * chore: 필요한 라이브러리 추가 및 tailwind 설정
    
    * chore: 필요한 라이브러리 추가 및 tailwind 설정
    
    * chore: tailwind 설정 추가
    
    * chore: storybook 추가
    
    * chore: eslint 설정 추가
    
    * chore: 절대 경로 설정
    
    * chore: 절대경로로 tailwind.css import
    
    * chore: 페이지 임시 구현
    
    * chore: mono repo 관리를 위한 script 추가
    
    * chore: 이슈, PR 템플릿 및 배포 스크립트 추가

---
+++ client/.eslintrc.js
@@ -0,0 +1,73 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+    es6: true,
+  },
+  plugins: ['@typescript-eslint', 'simple-import-sort', 'unused-imports'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@next/next/recommended',
+    'plugin:prettier/recommended',
+    'next',
+    'next/core-web-vitals',
+    'plugin:storybook/recommended',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'off',
+      {
+        endOfLine: 'auto',
+        singleQuote: true,
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+    'react/prop-types': 0,
+    'no-console': 'warn',
+    'no-unused-vars': 0,
+    'react/no-unescaped-entities': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+    'simple-import-sort/imports': [
+      'warn',
+      {
+        groups: [
+          ['^@?\\w', '^\\u0000'],
+          ['^.+\\.s?css$'],
+          ['^@/lib', '^@/hooks'],
+          ['^@/data'],
+          ['^@/components'],
+          ['^@/'],
+          [
+            '^\\./?$',
+            '^\\.(?!/?$)',
+            '^\\.\\./?$',
+            '^\\.\\.(?!/?$)',
+            '^\\.\\./\\.\\./?$',
+            '^\\.\\./\\.\\.(?!/?$)',
+            '^\\.\\./\\.\\./\\.\\./?$',
+            '^\\.\\./\\.\\./\\.\\.(?!/?$)',
+          ],
+          ['^'],
+        ],
+      },
+    ],
+  },
+};
---
+++ client/.storybook/main.js
@@ -0,0 +1,45 @@
+/*
+module.exports = {
+  "stories": [
+    "../stories/!**!/!*.stories.mdx",
+    "../stories/!**!/!*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}*/
+
+//.storybook/main.js
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  /** Expose public folder to storybook as static */
+  staticDir: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      /**
+       * Fix Storybook issue with PostCSS@8
+       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
+       */
+      //postcss를 활용할 수 있도록 이 부분 추가.
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+  // webpack5를 사용할 수 있도록 이 부분 추가
+  core: {
+    builder: 'webpack5',
+  },
+};
